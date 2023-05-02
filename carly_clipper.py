d73d34e3648e39ca6d60ccce57497323d63a0da4
Cyber Range Student <student@ip-10-1-57-163.ec2.internal>
2023-05-02 19:42:18 +0000

    carly_clipper

---
+++ carly_clipper.py
@@ -0,0 +1,84 @@
+#!/bin/python3
+
+# Step 1
+
+total_price = 0
+
+
+
+# Step 2
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+for price in prices:
+
+    total_price += price
+
+
+
+# Step 3
+
+average_price = total_price / len(prices)
+
+
+
+# Step 4
+
+print("Average Haircut Price: {}".format(average_price))
+
+
+
+# Step 5
+
+new_prices = [price - 5 for price in prices]
+
+
+
+# Step 6
+
+print(new_prices)
+
+
+
+# Step 7
+
+total_revenue = 0
+
+
+
+# Step 8
+
+for i in range(len(hairstyles)):
+
+    # Step 9
+
+    total_revenue += prices[i] * last_week[i]
+
+
+
+# Step 10
+
+print("Total Revenue: {}".format(total_revenue))
+
+
+
+# Step 11
+
+average_daily_revenue = total_revenue / 7
+
+print("Average Daily Revenue: {}".format(average_daily_revenue))
+
+
+
+# Step 12
+
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)) if new_prices[i] < 30]
+
+
+
+# Step 13
+
+print(cuts_under_30)
+
+
+done
